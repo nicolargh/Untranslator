18dbc19809639397a667edec63fa2f576e62b0f6
Nicola Gibson <nigibso@yammer-inc.com>
2020-01-19 19:42:00 -0800

    Add html parsing to translation result

--- app/src/main/java/com/example/untranslator/data/TranslationApi.kt
+++ app/src/main/java/com/example/untranslator/data/TranslationApi.kt
@@ -1,10 +1,12 @@
 package com.example.untranslator.data
 
+import android.os.Build
+import android.text.Html
+import androidx.core.text.HtmlCompat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.Exception
 import java.net.URL
 import java.net.URLEncoder
 
@@ -17,6 +19,7 @@ class TranslationApi {
     companion object {
         // google apps script of mine that handles translation
         // view script here: https://script.google.com/d/1YrUKgBhlEAkn1zRLAk5j5qGmR5gTNKNLuAtBvxKXIFwFIFYbaZR9dpaE/edit?usp=sharing
+        // found on stack overflow: https://stackoverflow.com/a/48159904/12745149
         private const val TRANSLATE_URL =
             "https://script.google.com/macros/s/AKfycbygX9ACBLFFp2acIqzBa6Sl36ZYioq8fq3C5e8xo0rVzOffJjNz/exec"
         private const val ERROR_IDENTIFIER = "<title>Error</title>"
@@ -51,7 +54,13 @@ class TranslationApi {
         return if (response.toString().contains(ERROR_IDENTIFIER)) {
             TranslationResult.Error
         } else {
-            TranslationResult.Success(response.toString())
+            TranslationResult.Success(
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    Html.fromHtml(response.toString(), HtmlCompat.FROM_HTML_MODE_COMPACT).toString()
+                } else {
+                    Html.fromHtml(response.toString()).toString()
+                }
+            )
         }
     }
 
